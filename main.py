311bf71b9a02b7b11290f2e7baf6e093e16b0c25
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 15:58:15 +0700

    --- hoangkss5 ---

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import time
+# Load and preview dataset
+import datetime
+import pandas as pd
+import numpy as np
+
+day = 24 * 60 * 60
+year = 365.2425 * day
+
+
+def load_dataframe() -> pd.DataFrame:
+    """ Create a time series x sin wave dataframe. """
+    df = pd.DataFrame(columns=['date', 'sin'])
+    df.date = pd.date_range(start='2018-01-01', end='2021-03-01', freq='D')
+    df.sin = 1 + np.sin(df.date.astype('int64') // 1e9 * (2 * np.pi / year))
+    df.sin = (df.sin * 100).round(2)
+    df.date = df.date.apply(lambda d: d.strftime('%Y-%m-%d'))
+    return df
+
+train_df = load_dataframe()
